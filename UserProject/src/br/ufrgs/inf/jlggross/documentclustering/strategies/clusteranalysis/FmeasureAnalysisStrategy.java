299c2aa96dff900ca6deb1e356c86b3e8d231fdc
João Gross <joaoluizgg@gmail.com>
2014-07-09 22:16:09 -0300

    FmeasureAnalysisStrategy implemented. Before it was not working properly.
    Precision and recall, as well as F-measure, have been studied in the
    following materials:
    
    - http://en.wikipedia.org/wiki/Precision_and_recall - F-measure is well
    explained.
    - http://www-users.cs.umn.edu/~kumar/dmbook/ch8.pdf - Page 64 has a
    great example of precision and recall.

--- UserProject/src/br/ufrgs/inf/jlggross/documentclustering/strategies/clusteranalysis/FmeasureAnalysisStrategy.java
+++ UserProject/src/br/ufrgs/inf/jlggross/documentclustering/strategies/clusteranalysis/FmeasureAnalysisStrategy.java
@@ -1,78 +1,167 @@
 package br.ufrgs.inf.jlggross.documentclustering.strategies.clusteranalysis;
 
-import java.util.HashSet;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 import br.ufrgs.inf.jlggross.clustering.DataCluster;
 import br.ufrgs.inf.jlggross.clustering.DataObject;
+import br.ufrgs.inf.jlggross.clustering.strategy.AnalysisStrategy;
+import br.ufrgs.inf.jlggross.documentclustering.Document;
 
-public class FmeasureAnalysisStrategy {
+/* ---------------------------------------------------------------------------------
+ * 
+ * Precision, Recall and F-Measure:
+ * 1. Inform the expected classes of information (analysis with external information
+ * - supervised analysis).
+ * 2. Given the clusters, calculated by using an clustering algorithm, check how many
+ * data objects of each class that a cluster have.
+ * 3. Given a cluster, its precision and recall will be calculated considering the 
+ * class to which it has the higher amount of data objects as reference. Example: If
+ * a cluster has 2 data objects from class SERIES and 3 from class MOVIES, then the 
+ * calculation will be based on the MOVIES class.      
+ * 
+ * ---------------------------------------------------------------------------------
+ * Model of file for the classes:
+ * (line 01) Animals: 2
+ * (line 02) Cat.txt
+ * (line 03) Dog.txt
+ * (line 04)
+ * (line 05) Languages: 3
+ * (line 06) English.txt
+ * (line 07) Portuguese.txt
+ * (line 08)
+ * (line 09) Series: 3
+ * (line 10) Fringe.txt
+ * (line 11) Supernatural.txt
+ * (line 12) The-Walking-Dead.txt
+ * 
+ * An example can also be find in data/test01-wikipedia12/classes.txt
+ * ---------------------------------------------------------------------------------
+ */
 
-	// TODO Try to pass some classes to test it!
-	public double executeAnalysis(List<DataObject> objects, List<DataCluster> dataClusters, List<DataCluster> dataClasses) {
-		double fmeasure = 0.0;
-		int iterations = dataClusters.size();
+public class FmeasureAnalysisStrategy extends AnalysisStrategy {
 
-		// For each cluster, finds out what class have most of its objects.
-		for (DataCluster cluster : dataClusters) {
-			int classIndex = -1;
-			int classIntersec = 0;
-			for (int i = 0; i < dataClasses.size(); i++) {
+	List<List<String>> dataClasses;
+	
+	/**
+	 * Definition: F-Measure Analysis Constructor
+	 * 
+	 * @param filename : refers to a file with the expected classes. These classes
+	 * are a reference for the clusters that the clustering algorithm must return. 
+	 */
+	public FmeasureAnalysisStrategy(String filename) {
+		// Initialize the data classes
+		this.dataClasses = new ArrayList<List<String>>();
+		this.dataClasses = this.loadDataClasses(filename);
+	}
+	
+	
+	/**
+	 * Definition: F-Measure core algorithm execution.
+	 * 
+	 * @param dataObjects : list of data objects.
+	 * @param dataClusters : list of data clusters. These clusters were calculated using a 
+	 * clustering algorithm.  
+	 */
+	@Override
+	public double executeAnalysis(List<DataObject> dataObjects, List<DataCluster> dataClusters) {
+		
+		// First gets the data objects names of each cluster
+		List<List<String>> clusters = new ArrayList<List<String>>();
+		for (DataCluster c : dataClusters) {
+			List<String> newCluster = new ArrayList<String>();
+			int size = c.getDataObjects().size();
+			for (int i = 0; i < size; i++) {
+				Document doc = new Document("doc", "", 0);
+				doc = (Document) c.getObject(i);
+				newCluster.add(doc.getTitle());
+			}
+			clusters.add(newCluster);
+		}
+		
+		// Find the reference class for each cluster and calculate precision and recall	
+		double[] precision = new double[dataClusters.size()];
+		double[] recall = new double[dataClusters.size()];
+		double[] fmeasure = new double[dataClusters.size()];
+		
+		int clusterIndex = 0;
+		for (List<String> cluster : clusters) {
+			int currentClassIndex = 0;
+			int selectedClassIndex = -1;
+			int max = -1;
+			for (List<String> aClass : this.dataClasses) {
+				int size = cluster.size();
 				int count = 0;
-				List<DataObject> classObjects = dataClasses.get(i).getDataObjects();
-				for (DataObject object : cluster.getDataObjects()) {
-					if (classObjects.contains(object)) {
+				for (int i = 0; i < size; i++) {
+					String s = cluster.get(i);
+					if (aClass.contains(s)) {
 						count++;
 					}
 				}
-				if (count > classIntersec) {
-					classIntersec = count;
-					classIndex = i;
+				
+				if (count > max) {
+					selectedClassIndex = currentClassIndex;
+					max = count;
+				}
+				currentClassIndex++;
+			}
+						
+			precision[clusterIndex] = max / cluster.size();
+			recall[clusterIndex] = max / this.dataClasses.get(selectedClassIndex).size();
+			fmeasure[clusterIndex] = fmeasure(precision[clusterIndex], recall[clusterIndex]);
+			clusterIndex++;
+		}
+		
+		return 0;
+	}
+	
+	
+	/**
+	 * Definition: Load the data classes.
+	 * 
+	 * @param filename : refers to a file with the expected classes. 
+	 */
+	private List<List<String>> loadDataClasses(String filename) {
+		
+		List<List<String>> classes = new ArrayList<List<String>>();
+		
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(filename));
+			String line = "";
+			while (null != (line = reader.readLine())) {
+				if (!line.isEmpty()) {
+					
+					String[] parse = line.split(": ");
+					List<String> newClass = new ArrayList<String>();
+					int count = Integer.valueOf(parse[1]);
+					
+					for (int i = 0; i < count; i++) {
+						line = reader.readLine();
+						newClass.add(line);
+					}
+					
+					line = reader.readLine();
+					classes.add(newClass);
 				}
 			}
-			
-			if (classIndex != -1) {
-				fmeasure += this.fmeasure(cluster, dataClasses.get(classIndex));
-			}
+			reader.close();
+		} catch (IOException e) {
+			System.out.println("IOException in function loadMatrix of class Matrix2D.java");
+			e.printStackTrace();
 		}
 		
-		return fmeasure / iterations;
+		return classes;
 	}
 	
-	private double fmeasure(DataCluster cluster, DataCluster clazz) {
-		return 2 * ((this.precision(cluster, clazz) * this.recall(cluster, clazz))
-				/ (this.precision(cluster, clazz) + this.recall(cluster, clazz)));
+	
+	private double fmeasure(double precision, double recall) {
+		return 2 * ((precision * recall) / (precision + recall));
 	}
 	
-	private double precision(DataCluster cluster, DataCluster clazz) {
-		int count = 0;
-		
-		for (DataObject object : cluster.getDataObjects()) {
-			if (clazz.getDataObjects().contains(object)) {
-				count++;
-			}
-		}
-		
-		return (double) count / cluster.getDataObjects().size();
-	}
-	
-	private double recall(DataCluster cluster, DataCluster clazz) {
-		int count = 0;
-		
-		for (DataObject object : cluster.getDataObjects()) {
-			if (clazz.getDataObjects().contains(object)) {
-				count++;
-			}
-		}
-		
-		return (double) count / clazz.getDataObjects().size();
-	}
-	
-	public double microaveragePrecision(List<DataCluster> dataClusters, List<DataCluster> dataClasses) {
-		return 0.0;
-	}
-	
+	/* TODO
 	public double microaverageRecall(List<DataCluster> dataClusters, List<DataCluster> dataClasses) {
 		double clustered = 0.0;
 		Set<DataObject> classifiedObjects = new HashSet<DataObject>();
@@ -89,5 +178,6 @@ public class FmeasureAnalysisStrategy {
 		
 		return clustered / classified;
 	}
+	*/
 
 }
